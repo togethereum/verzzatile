7052575817303a1fab697a7d031039ed0c981a33
Adam Schmideg <adamschmideg@users.noreply.github.com>
2024-03-26 22:11:00 +0100

    Trying to add Babel but it does not help

--- restart/src/types/db.ts
+++ restart/src/types/db.ts
@@ -1,7 +1,6 @@
 import { JSONFileSync } from 'lowdb/node';
 import { LowSync } from 'lowdb';
 
-
 export function roundtripDb(filename: string, defaultData: object = {}, key: string, value: string): string {
     const db = new LowSync<any>(new JSONFileSync(filename), defaultData);
     db.data[key] = value;
